478c5981cd2cd4902f49cb3d6b723b8bdbf48908
baipeng <fredomjustice66@gmail.com>
2020-12-30 11:01:55 +0800

    dev - 333

--- BPGBanner/AppDelegate.h
+++ BPGBanner/AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by baipeng on 2017/6/8.
 //  Copyright © 2017年 BPG. All rights reserved.
 //
-
+//333
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
